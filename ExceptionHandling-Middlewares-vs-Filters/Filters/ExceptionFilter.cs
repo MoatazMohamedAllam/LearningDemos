f9edb654dc5b22e4cf8a445b2925c98caf511b25
Moataz Allam <mallam@diginationmea.com>
2023-12-02 21:00:40 +0200

    filters vs middleware for exception handling

---
+++ ExceptionHandling-Middlewares-vs-Filters/Filters/ExceptionFilter.cs
@@ -0,0 +1,20 @@
+﻿using ExceptionHandling_Middlewares_vs_Filters.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace ExceptionHandling_Middlewares_vs_Filters.Filters
+{
+    public class ExceptionFilter : IExceptionFilter
+    {
+        public void OnException(ExceptionContext context)
+        {
+            var error = new Error
+            {
+                StatusCode = "500",
+                Message = context.Exception.Message,
+            };
+
+            context.Result = new JsonResult(error) { StatusCode = 500 };
+        }
+    }
+}
---
+++ ExceptionHandling-Middlewares-vs-Filters/Middlewares/ExceptionMiddleware.cs
@@ -0,0 +1,34 @@
+﻿using ExceptionHandling_Middlewares_vs_Filters.Contracts;
+using System.Net;
+
+namespace ExceptionHandling_Middlewares_vs_Filters.Middlewares
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _requestDelegate;
+
+        public ExceptionMiddleware(RequestDelegate requestDelegate)
+        {
+            _requestDelegate = requestDelegate;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _requestDelegate(context);
+            }
+            catch (Exception ex)
+            {
+                context.Response.ContentType = "application/json";
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                var error = new Error
+                {
+                    Message = ex.Message,
+                    StatusCode = context.Response.StatusCode.ToString(),
+                };
+                await context.Response.WriteAsync(error.ToString());
+            }
+        }
+    }
+}
